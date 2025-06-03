76017f12444453127b52985f5175bcd917727603
Malay Panara <160609243+Malay911@users.noreply.github.com>
2025-06-03 09:27:17 +0530

    Add files via upload

---
+++ matrixaddition.java
@@ -0,0 +1,25 @@
+//print addition of two matrix
+import java.util.Scanner;
+public class matrixaddition{
+    public static void main(String[] args){
+        Scanner sc=new Scanner(System.in);
+        int[][] arr=new int[3][3];
+        int[][] brr=new int[3][3];
+        int[][] crr=new int[3][3];
+        // System.out.println("Enter the elements of the array: ");
+        for(int i=0;i<crr.length;i++){
+            for(int j=0;j<crr.length;j++){
+                arr[i][j]=sc.nextInt();
+                brr[i][j]=sc.nextInt();
+                crr[i][j]=arr[i][j]+brr[i][j];
+                System.out.println("Enter the elements of the array: ");
+            }
+        }
+        for(int i[]:crr){
+            for(int j:i){
+                System.out.print(j+" ");
+            }
+            System.out.println("");
+        }
+    }
+}
