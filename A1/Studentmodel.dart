425933476000ceb90305c5344f396b7722847e00
Malay Panara <160609243+Malay911@users.noreply.github.com>
2025-06-05 16:49:21 +0530

    Add files via upload

---
+++ A1/Studentmodel.dart
@@ -0,0 +1,9 @@
+class StudentModel {
+  String name;
+  String enrollmentNo;
+
+  StudentModel({
+    required this.name,
+    required this.enrollmentNo,
+  });
+}
---
+++ A1/utils/import_export.dart
@@ -0,0 +1,3 @@
+export 'package:lab/Lab 1/A1/Studentmodel.dart';
+export 'package:flutter/material.dart';
+export 'package:lab/Lab 1/A1/Studentcontroller.dart';
