5e8a11071abae4fb3a61be1c3473f3b6624959f2
Malay Panara <160609243+Malay911@users.noreply.github.com>
2025-06-15 09:56:54 +0530

    Add files via upload

---
+++ Lab 1/A1/Studentmodel.dart
@@ -0,0 +1,15 @@
+// 1. WAP to create class Student With a method to display student details.
+// 2. WAP implement inheritance a class to inherit properties & methods from another class.
+// 3. WAP for implementation of a method overriding.
+// 4. WAP to create abstract classes & methods to define in child class.
+// 5. WAP to create static Variable & call It from two different classes & observe the result.
+
+class StudentModel {
+  String name;
+  String enrollmentNo;
+
+  StudentModel({
+    required this.name,
+    required this.enrollmentNo,
+  });
+}
