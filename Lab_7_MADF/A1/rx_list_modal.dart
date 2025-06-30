a7f788c7f148591fdbc1457d8093041bee071e54
Malay Panara <160609243+Malay911@users.noreply.github.com>
2025-06-30 09:27:20 +0530

    Delete Lab_7_MADF/A1 directory

--- Lab_7_MADF/A1/rx_list_modal.dart
+++
@@ -1,6 +0,0 @@
-// WAP to display the list in ListView using RXList.
-class ItemModel {
-  final String name;
-
-  ItemModel({required this.name});
-}
--- Lab_7_MADF/A1/utils/import_export.dart
+++
@@ -1,4 +0,0 @@
-export 'package:flutter/material.dart';
-export 'package:get/get.dart';
-export 'package:lab/Lab_7_MADF/A1/rx_list_modal.dart';
-export 'package:lab/Lab_7_MADF/A1/rx_list_controller.dart';
