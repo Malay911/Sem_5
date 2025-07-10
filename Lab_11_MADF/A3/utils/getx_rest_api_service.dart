9e75459672e75a33a8a9e77a2ebdc86b63c29930
Malay Panara <160609243+Malay911@users.noreply.github.com>
2025-07-10 19:00:32 +0530

    Add files via upload

---
+++ Lab_11_MADF/A3/utils/getx_rest_api_service.dart
@@ -0,0 +1,18 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:lab/Lab_11_MADF/A3/getx_rest_api_search_model.dart';
+
+class GetxRestApiService {
+  static const String baseUrl =
+      'https://67c3db4c89e47db83dd2a1c3.mockapi.io/API_CRUD';
+
+  Future<List<GetxRestApiModel>> fetchData() async {
+    final response = await http.get(Uri.parse(baseUrl));
+    if (response.statusCode == 200) {
+      List<dynamic> data = jsonDecode(response.body);
+      return data.map((item) => GetxRestApiModel.fromMap(item)).toList();
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
+}
